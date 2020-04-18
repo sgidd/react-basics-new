954866da3efbf97bcf8bf5844789aba85ffc44e9
sunil <sunilgidd051@gmail.com>
2020-04-18 12:13:40 +0530

    First commit

---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const person = (props2) => {
+    //return <p>I'm a Person and I'am {Math.floor(Math.random() * 30) } years old!</p>
+    return (
+        <div>
+            <p>I'm a {props2.name} and I'am {props2.age } years old!</p>
+            <p>{props2.children}</p>
+        </div>
+    );
+}
+
+export default person;
