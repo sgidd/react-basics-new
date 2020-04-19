527315c457523ffc4455662b398f7a2e89f6226e
sunil <sunilgidd051@gmail.com>
2020-04-19 11:11:54 +0530

    adding stylesheet and inline styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './Person.css';
+
 const person = (props2) => {
     //return <p>I'm a Person and I'am {Math.floor(Math.random() * 30) } years old!</p>
     return (
-        <div>
+        <div className="Person">
             <p onClick={() => props2.click('SunilGidd2')}>I'm a {props2.name} and I'am {props2.age } years old!</p>
             <p>{props2.children}</p>
             <input type="text" onChange={props2.changed} value={props2.name}/>
