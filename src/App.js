cd5e6ef989fcd021427395bb7493bf60a43e7d05
sunil <sunilgidd051@gmail.com>
2020-04-19 09:50:06 +0530

    using useState() hook for state manipulation

--- src/App.js
+++ src/App.js
@@ -1,51 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
-
-class App extends Component {
-  state ={
-    persons :[
-      {name:"Sunil" , age:26},
-      {name:"Manoj" , age:267},
-      {name:"PJ" , age:28}
-    ],
-  }
-
-  swicthNameHandler = () => {
-    // console.log("button clicked");
-    //this.state.persons[0].name = "SunilG" dont do this
-    this.setState({
-      persons: [
-        {name:"SunilG" , age:26},
+const app = (props) => {
+    const [personsState, setPersonsState] = useState({
+      persons :[
+        {name:"Sunil" , age:26},
         {name:"Manoj" , age:267},
         {name:"PJ" , age:28}
-      ]
+      ],
+      otherState : "some other value"
     })
-  }
-  render() {
+
+    const [otherState, setOtherState] = useState({otherState : "some other value"});
+    console.log(personsState, otherState);
+
+    const swicthNameHandler = () => {
+      setPersonsState({
+        persons :[
+          {name:"sunilGidd" , age:26},
+          {name:"Manoj" , age:267},
+          {name:"PJ" , age:29}
+        ],
+        // otherState: personsState.otherState
+      })    
+    }
     return (
         <div className="App">
           <h1>Hi, I'm a React App</h1>
-          <button onClick={this.swicthNameHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbie is : Racing </Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        
+          <button onClick={swicthNameHandler}>Switch Name</button>
+          <Person 
+          name={personsState.persons[0].name} 
+          age={personsState.persons[0].age}/>
+
+          <Person 
+          name={personsState.persons[1].name} 
+          age={personsState.persons[1].age}
+          click={swicthNameHandler}>My Hobbie is : Racing </Person>
+
+          <Person 
+          name={personsState.persons[2].name} 
+          age={personsState.persons[2].age}/>      
         </div>
-    );
-
-    // return React.createElement('div' , null, 'h1', 'Hi, I\'m a React App');
-    // first arg is element tag , 2nd is configuration - style , classes , 3rd is the content or child elemnts
-    // above statement takes he as content not element
-    // h1Hi, I'm a React App
-    // so to create child element use createElement 
-
-    //return React.createElement('div' , null, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
-    //output : Hi, I'm a React App!!!
-
-    // adding classes
-    // return React.createElement('div' , {className : 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
-  }
+    );  
 }
+export default app;
 
-export default App;
+
+
+// const stateArr = useState({
+//   persons :[
+//     {name:"Sunil" , age:26},
+//     {name:"Manoj" , age:267},
+//     {name:"PJ" , age:28}
+//   ],
+//   otherState : "some other value"
+// })
+//   const swicthNameHandler = () => {
+//     stateArr[1]({
+//       persons :[
+//         {name:"SunilG" , age:26},
+//         {name:"Manoj" , age:267},
+//         {name:"PJ" , age:28}
+//       ],
+//     })    
+//   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ const person = (props2) => {
     //return <p>I'm a Person and I'am {Math.floor(Math.random() * 30) } years old!</p>
     return (
         <div>
-            <p>I'm a {props2.name} and I'am {props2.age } years old!</p>
+            <p onClick={props2.click}>I'm a {props2.name} and I'am {props2.age } years old!</p>
             <p>{props2.children}</p>
         </div>
     );
