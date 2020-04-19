4cb811fdb37d0c527cfeda25d46ae29925d17254
sunil <sunilgidd051@gmail.com>
2020-04-20 01:10:06 +0530

    Outputting Lists

--- src/App.js
+++ src/App.js
@@ -48,22 +48,16 @@ class App extends Component {
       padding: '8px',
       cursor:'pointer'
     }
-
     let persons = null;
     if(this.state.showPersons){
       persons = (
         <div>
-          <Person   name={this.state.persons[0].name}   age={this.state.persons[0].age}/>
-            <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            click={() => this.swicthNameHandler('SunilGidd')}
-            changed={this.nameChangedHandler}>My Hobbie is : Racing </Person>
-
-            <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}
-            click={(fromChild) => this.swicthNameHandler(fromChild)}/>
+          {
+            this.state.persons.map( person => {
+              return <Person name= {person.name} age={person.age}/>
+            })
+          }
+         
         </div>
       );
 
